dde12b6f966ec75dbed84cd3e06db37b377b3604
Ceren Arslan <ceren.arslan2@ogr.gelisim.edu.tr>
2025-03-03 15:39:29 +0300

    Add files via upload

---
+++ cookie.php
@@ -0,0 +1,18 @@
+<?php
+// setcookie("cerez","Ceren",time()+(60*60*24*365));
+// if(isset($_COOKIE["cerez"]))
+// {
+//     echo "Çerezin Adı: ".$_COOKIE["cerez"];
+// }
+// else
+// {
+//     echo "Böyle bir çerez yok";
+// }
+
+
+
+
+
+
+
+?>
