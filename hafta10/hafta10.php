461aeefe03fcf4fd79bc531993a2a20b789d43bb
Ceren Arslan <ceren.arslan2@ogr.gelisim.edu.tr>
2025-05-05 15:28:47 +0300

    Add files via upload

---
+++ hafta10/hafta10.php
@@ -0,0 +1,103 @@
+<?php
+
+// $pattern="#Gelişim#";
+// $metin="Ben Gelişim Üniversitesinde Web Tasarım ve Kodlama Öğrencisiyim."."<br>";
+// echo $metin."Aranılan kelime <u>Gelişim</u>"."<br>";
+// if(preg_match($pattern,$metin,$sonuc))
+// {
+//     echo "Aranılan kelime mevcut";
+// }
+
+// else
+// {
+//     echo "Aranılan kelime mevcut değil";
+// }
+// echo "<pre>";
+// print_r($sonuc);
+// echo "</pre>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $pattern="/[0-9]+/";
+// $metin="Şu an 4. dönemdesiniz."."<br>";
+// if(preg_match($pattern,$metin,$sonuc))
+// {
+//     echo "Metin içerisinde en az bir rakam mevcut";
+// }
+
+// else
+// {
+//     echo "Metin içerisinde rakam mevcut değil";
+// }
+// echo "<pre>";
+// print_r($sonuc);
+// echo "</pre>";
+
+
+
+
+
+
+
+
+
+
+
+//saatkontrolü
+
+// $pattern="/^([01][0-9]|[2][0-3]):[0-5][0-9]:[0-5][0-9]$/";
+// $saat="22:00:54";
+// if(preg_match($pattern,$saat))
+// {
+//     echo "Doğru bir saat formatı girilmiş";
+// }
+
+// else 
+
+// {
+//     echo "Yanlış bir saat formatı girilmiş";
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
